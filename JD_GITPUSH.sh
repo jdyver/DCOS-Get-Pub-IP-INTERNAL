ada4096059aa5ba50d06440f99c05528962ffef0
James Dyckowski <jdyver@gmail.com>
2019-01-26 10:16:31 -0500

    JD-GIT AUTO:

--- JD_GITPUSH.sh
+++
@@ -1,9 +0,0 @@
-# Generic Update Push to PWD's Github Repo
-echo " JD-GIT: Message is $1 $2 $3 $4 $5"
-
-git add -A
-git commit -am "JD-GIT AUTO: $1 $2 $3 $4 $5"
-git push
-
-echo " JD-GIT: This was pushed to:"
-git branch
