b02cb509fd3be3571ad60d2cc62b69dcd5152933
James Dyckowski <jdyver@gmail.com>
2019-01-26 00:51:07 -0500

    JD-GIT AUTO:

--- get-dcos-public-agent-ip_v2.sh
+++ get-dcos-public-agent-ip_v2.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #
-# SCRIPT:   get-dcos-public-agent-ip.sh
+# SCRIPT:   get-dcos-public-agent-ip_v2.sh
 #
 # DESCR:    Get the Amazon Public IP Address for the public DCOS agent nodes. If
 #           no arguments are supplied it will attempt to start on 2 pubic agent nodes.
 #
-# USAGE:    get-dcos-public-agent-ip.sh <num-pub-agents> <--mlb: Marathon-LB check or add>
+# USAGE:    get-dcos-public-agent-ip.sh <num-pub-agents> <--mlb: Marathon-LB check or add> <--elb: Edge-LB check or add> <elb-local.cfg.json location>
+#
+# - get-dcos-public-agent-ip.sh
+# - get-dcos-public-agent-ip.sh 5
+# - get-dcos-public-agent-ip.sh --mlb
+# - get-dcos-public-agent-ip.sh --elb /Users/username/Documents/Scripts/edgelb.cfg.json
 #
 
 echo
@@ -14,20 +19,42 @@ then
     num_pub_agents=2
     echo " Using the default number of public agent nodes (2)"
     mlb_enabled=0
+    elb_enabled=0
+    elb_cfg_file=0
 elif [ $1 == "--mlb" ]
 then
     mlb_enabled=1
+    elb_enabled=0
+    elb_cfg_file=0
     num_pub_agents=2
     echo " Using the default number of public agent nodes (2) with Marathon-LB"
+elif [ $1 == "--elb" ]
+then
+    elb_enabled=1
+    elb_cfg_file=$2
+    mlb_enabled=0
+    num_pub_agents=2
+    echo " Using the default number of public agent nodes (2) with Edge-LB:$2"
 elif [ "$2" == "--mlb" ]
 then
     mlb_enabled=1
+    elb_enabled=0
+    elb_cfg_file=0
     num_pub_agents=$1
     echo " Using $num_pub_agents public agent node(s) with Marathon-LB"
+elif [ "$2" == "--elb" ]
+then
+    mlb_enabled=0
+    elb_enabled=1
+    elb_cfg_file=$3
+    num_pub_agents=$1
+    echo " Using $num_pub_agents public agent node(s) with Edge-LB:$3"
 else
     num_pub_agents=$1
     echo " Using $num_pub_agents public agent node(s)"
     mlb_enabled=0
+    elb_enable=0
+    elb_cfg_file=0
 fi
 
 # get the public IP of the public node if unset
@@ -83,7 +110,37 @@ else
     echo
 fi
 
-# End of M-LB Check #################################################################
+# End of M-LB Check ##############################################
+
+###### EDGE-LB ###################################################
+# --elb flag #####################################################
+
+if [ "$elb_enabled" == "1" ]
+then
+    marathon=`dcos marathon app list`
+
+               # JD # dcos marathon app list
+               # ID                               MEM   CPUS  TASKS  HEALTH  DEPLOYMENT  WAITING  CONTAINER  CMD
+               # /dcos-edgelb/api                 1024   1     1/1    1/1       ---      False      MESOS    cp -vR /dcosfilestmp/*...
+               # /dcos-edgelb/pools/edgelb-proxy  2048   1     1/1    1/1       ---      False      MESOS    export...
+
+    if [[ $marathon == *edgelb-proxy* ]]
+    then
+        echo
+    else
+        echo
+        echo " Edge-LB Not Found: Deploying"
+        echo
+        dcos package install --yes edgelb &> /dev/null
+        sleep 15 							#!!!! CHECK THIS TIMING
+	dcos edgelb create $elb_cfg_file
+	sleep 15 							#!!!! CHECK THIS TIMING
+    fi
+else
+    echo
+fi
+
+# End of EDGE-LB Check ################################################
 
 task_list=`dcos task get-public-agent-ip | grep get-public-agent-ip | awk '{print $5}'`
 
